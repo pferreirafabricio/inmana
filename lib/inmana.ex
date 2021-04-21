f9a6b6318ec2a2a0f7d0f67544ffa513118a670d
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-04-21 11:15:19 -0300

    feat(supply): create show logic

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,8 +1,10 @@
 defmodule Inmana do
   alias Inmana.Restaurants.Create, as: RestaurantCreate
   alias Inmana.Supplies.Create, as: SupplyCreate
+  alias Inmana.Supplies.Show, as: SupplyShow
 
   defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
 
   defdelegate create_supply(params), to: SupplyCreate, as: :call
+  defdelegate show_supply(uuid), to: SupplyShow, as: :call
 end
