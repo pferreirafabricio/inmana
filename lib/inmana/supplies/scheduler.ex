569a1928def36e0738331ee088ae1203dbada14b
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-04-22 10:17:06 -0300

    feat(email): implements GenServer to send

--- lib/inmana/supplies/scheduler.ex
+++ lib/inmana/supplies/scheduler.ex
@@ -1,16 +1,30 @@
 defmodule Inmana.Supplies.Scheduler do
   use GenServer
 
+  alias Inmana.Supplies.ExpirationNotification
+
+  # CLIENT
+
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # SERVER
+
+  @impl true
   def init(state \\ %{}) do
+    schedule_notification()
     {:ok, state}
   end
 
-  # async
-  def handle_cast({:put, key, value}, state) do
-    {:noreply, Map.put(state, key, value)}
+  @impl true
+  def handle_info(:generate, state) do
+    ExpirationNotification.send()
+    schedule_notification()
+    {:noreply, state}
   end
 
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key), state}
+  defp schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 60 * 60 * 24 * 7)
   end
 end
